b589836546b97b3915ff151ae8e98b04448e9d8a
Lurz0205 <91960598+Lurz0205@users.noreply.github.com>
2023-09-25 19:24:56 +0700

    Update flappybird.js

--- flappybird.js
+++ flappybird.js
@@ -1,38 +1,52 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const bird = document.getElementById('bird');
-  const gameContainer = document.getElementById('game-container');
-  const scoreElement = document.getElementById('score');
+const puppeteer = require('puppeteer');
 
-  let birdTop = 220;
-  let score = 0;
-  let isGameOver = false;
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
 
-  function jump() {
-    if (birdTop > 20) {
-      birdTop -= 40;
-    }
-  }
+  await page.goto('file:///index.html'); // Thay thế đường dẫn tới tệp HTML của bạn
 
-  function startGame() {
-    if (isGameOver) return;
+  await page.evaluate(() => {
+    const bird = document.getElementById('bird');
+    const gameContainer = document.getElementById('game-container');
+    const scoreElement = document.getElementById('score');
 
-    bird.style.top = birdTop + 'px';
-    birdTop += 2;
+    let birdTop = 220;
+    let score = 0;
+    let isGameOver = false;
 
-    if (birdTop > gameContainer.clientHeight - 40) {
-      gameOver();
+    function jump() {
+      if (birdTop > 20) {
+        birdTop -= 40;
+      }
     }
 
-    scoreElement.innerText = 'Score: ' + score;
+    function startGame() {
+      if (isGameOver) return;
 
-    requestAnimationFrame(startGame);
-  }
+      bird.style.top = birdTop + 'px';
+      birdTop += 2;
 
-  function gameOver() {
-    isGameOver = true;
-    scoreElement.innerText = 'Game Over';
-  }
+      if (birdTop > gameContainer.clientHeight - 40) {
+        gameOver();
+      }
 
-  document.addEventListener('keydown', jump);
-  startGame();
-});
+      scoreElement.innerText = 'Score: ' + score;
+
+      requestAnimationFrame(startGame);
+    }
+
+    function gameOver() {
+      isGameOver = true;
+      scoreElement.innerText = 'Game Over';
+    }
+
+    document.addEventListener('keydown', jump);
+    startGame();
+  });
+
+  // Đợi một thời gian để trò chơi chạy
+  await page.waitForTimeout(5000);
+
+  await browser.close();
+})();
