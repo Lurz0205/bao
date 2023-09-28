e528869ad5585518ce7cb236abd195cc7bbff594
Lurz0205 <91960598+Lurz0205@users.noreply.github.com>
2023-09-28 17:21:01 +0700

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = 3000;
 const spotifyApi = new SpotifyWebApi({
   clientId: '40c5ee08678d4e60aef46bedc6761fd4',
   clientSecret: '2e8ef57a93454b31b035569f64fea7c8',
-  redirectUri: 'http://bao-lofimusic.onrender.com/callback' // URL chuyển hướng sau khi xác thực
+  redirectUri: 'https://bao-lofimusic.onrender.com/callback' // URL chuyển hướng sau khi xác thực
 });
 
 // Đăng nhập Spotify
